2a44592d6b900e71dd4d4e0e59c064b2087a7174
GeorgeKihara <gegohcomp@gmail.com>
2017-10-12 23:16:32 +0300

    app files and folders

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+from datetime import datetime
+from flask_pymongo import PyMongo
+
+#connections to the mongo database
+app.config['MONGO_DBNAME'] = 'bktlist'
+app.config['MONGO_URI'] = 'mongodb://kihara:kihara@ds151752.mlab.com:51752/bktlist'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1 style="text-align:center;">Welcome to EAZYTOI</h1>
+    <p>It is currently {time}.</p>
+
+    """.format(time=the_time)
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if  __name__ == '__main__':
+    app.secret_key='mysecret'
+	app.run(debug=True))

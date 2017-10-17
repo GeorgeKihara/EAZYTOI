8ec52a4ec3912ddcb4a696b1cae634115717f8f7
GeorgeKihara <gegohcomp@gmail.com>
2017-10-17 08:43:35 +0300

    maps config in the views file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, redirect
 from datetime import datetime
 from flask_pymongo import PyMongo
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+
+app = Flask(__name__, template_folder=".")
+GoogleMaps(app)
 
 #connections to the mongo database
 app.config['MONGO_DBNAME'] = 'bktlist'
@@ -20,7 +25,28 @@ def homepage():
     """.format(time=the_time)
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    #creating a map
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+            {
+                'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat':37.4300,
+                'lng':-122.1419,
+                'infobox':"<b>Here is eazytoi map</b>"
+            }
+        ]
+    )
+
+    return render_template('index.html', mymap=mymap, sndmap=sndmap)    
 
 if  __name__ == '__main__':
     app.secret_key='mysecret'
